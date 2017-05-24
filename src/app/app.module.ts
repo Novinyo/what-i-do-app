eebed60b83e678f27f54b2cc5ca1a61328b70867
Novinyo <novinyolearn@gmail.com>
2017-05-24 15:41:33 +0000

    What I do App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+   apiKey: 'AIzaSyCl7qL09rz5pZWO9GzAYhHiT6b4s1wOmfs',
+    authDomain: 'angular4test-ba127.firebaseapp.com',
+    databaseURL: 'https://angular4test-ba127.firebaseio.com',
+    projectId: 'angular4test-ba127',
+    storageBucket: 'angular4test-ba127.appspot.com',
+    messagingSenderId: '774022518113'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
